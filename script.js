ba2523d2fac2f036f4c9ad2f1a6d058b224c0d9e
Marikenyo <ongjullianrob27@gmail.com>
2025-11-27 09:36:38 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ function changeMap(place, element) {
     mapSrc = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d463.0772792907933!2d121.09342369368531!3d14.621809703344411!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b937e7e85f8d%3A0x428845390fc59fc8!2sBeanhi%20Coffee%20House!5e0!3m2!1sen!2sph!4v1757325016872!5m2!1sen!2sph";
     info = `
       <h2>Beanhi</h2>
-      <img src="/foodspot/beanhi.png" alt="Beanhi" style="width: 100%; height: 100%; max-height: 480px; border-radius: 10px; margin-bottom: 20px;">
+      <img src="foodspot/beanhi.png" alt="Beanhi" style="width: 100%; height: 100%; max-height: 480px; border-radius: 10px; margin-bottom: 20px;">
       <p><strong>Address:</strong> 83 M. H. del Pilar St, Marikina</p>
       <p><strong>Price Range:</strong> ₱70 - ₱125+</p>
       <p><strong>Description:</strong> A cozy coffee spot known for its premium iced coffee blends, non-coffee frappes, and refreshing booster juices. Perfect for a quick caffeine fix or a sweet treat.</p>
@@ -91,7 +91,7 @@ function changeMap(place, element) {
     mapSrc = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3859.9400574144815!2d121.1067260747813!3d14.659343185833995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b9d3bb77ec2d%3A0xfd58898bab836a06!2sBread%20Head%20Cafe!5e0!3m2!1sen!2sph!4v1757325138491!5m2!1sen!2sph"; 
     info = `
       <h2>Bread Head Cafe</h2>
-      <img src="/foodspot/breadhead.jpg" alt="Bread Head" style="width: 100%; height: 100%; max-height: 480px; border-radius: 10px; margin-bottom: 20px;">
+      <img src="foodspot/breadhead.jpg" alt="Bread Head" style="width: 100%; height: 100%; max-height: 480px; border-radius: 10px; margin-bottom: 20px;">
       <p><strong>Address:</strong> 9 Lakandula, Marikina</p>
       <p><strong>Price Range:</strong> ₱95 - ₱180+</p>
       <p><strong>Description:</strong> Famous for its freshly baked pastries and hearty all-day breakfast meals. They also serve savory pastas and signature coffee drinks in a warm, inviting atmosphere.</p>
@@ -100,7 +100,7 @@ function changeMap(place, element) {
     mapSrc = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1364.6943679433343!2d121.11075134124285!3d14.6594277430547!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b90fc50fd489%3A0x635765ebea3d6c98!2sDo%20G%C3%BCd%20Cafe!5e0!3m2!1sen!2sph!4v1757340327363!5m2!1sen!2sph";
     info = `
       <h2>Do Güd Cafe</h2>
-      <img src="/foodspot/dugud.jpg" alt="Do Güd Cafe" style="width: 100%; height: 100%; max-height: 480px; border-radius: 10px; margin-bottom: 20px;">
+      <img src="foodspot/dugud.jpg" alt="Do Güd Cafe" style="width: 100%; height: 100%; max-height: 480px; border-radius: 10px; margin-bottom: 20px;">
       <p><strong>Address:</strong> 9 E. Rodriguez St, Corner Lakandula, Marikina</p>
       <p><strong>Price Range:</strong> ₱105 - ₱200+</p>
       <p><strong>Description:</strong> A pet-friendly cafe offering a diverse menu of comfort foods, including rice bowls, specialty toasts, and a wide selection of coffee and matcha drinks.</p>
@@ -118,7 +118,7 @@ function changeMap(place, element) {
     mapSrc = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d573.7848084428898!2d121.10807462955331!3d14.658852280658971!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b9666645fc5f%3A0xdd2c39d0b0e954b6!2sCocol%20Coffee%20%26%20Tea%20-%20Parang%20Branch!5e0!3m2!1sen!2sph!4v1757325757793!5m2!1sen!2sph";
     info = `
       <h2>Cocol Coffee & Tea</h2>
-      <img src="/foodspot/cocol.png" alt="Cocol Coffee & Tea" style="width: 100%; height: 100%; max-height: 480px; border-radius: 10px; margin-bottom: 20px;">
+      <img src="foodspot/cocol.png" alt="Cocol Coffee & Tea" style="width: 100%; height: 100%; max-height: 480px; border-radius: 10px; margin-bottom: 20px;">
       <p><strong>Address:</strong> 18 Parnaso Street, Corner P. Paterno St, Marikina</p>
       <p><strong>Price Range:</strong> ₱100 - ₱200+</p>
       <p><strong>Description:</strong> Offers a wide variety of milk teas, fruit teas, and coffee blends. Pair your drink with their popular snacks like nachos, fries, or savory rice bowls.</p>
@@ -127,7 +127,7 @@ function changeMap(place, element) {
     mapSrc = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3859.928057809585!2d121.10803907523173!3d14.66002408583343!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b9912199739d%3A0x8d484652af20f1ca!2sDupleXpresso!5e0!3m2!1sen!2sph!4v1757341623663!5m2!1sen!2sph";
     info = `
       <h2>DupleXpresso</h2>
-      <img src="/foodspot/duplex.jpg" alt="DupleXpresso" style="width: 100%; height: 100%; max-height: 480px; border-radius: 10px">
+      <img src="foodspot/duplex.jpg" alt="DupleXpresso" style="width: 100%; height: 100%; max-height: 480px; border-radius: 10px">
       <p><strong>Address:</strong> 14 E Rodriguez, Marikina</p>
       <p><strong>Price Range:</strong> ₱90-200+</p>
       <p><strong>Description:</strong> A quaint and homey cafe ideal for hanging out. They serve affordable snacks, flavored fries, and refreshing drinks, often with board games available for customers.</p>
@@ -136,7 +136,7 @@ function changeMap(place, element) {
     mapSrc = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d965.1048867100061!2d121.099852!3d14.6321099!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b9be01931167%3A0x1d819863d42ceca8!2sKape%20Benito%20Community%20Cafe!5e0!3m2!1sen!2sph!4v1757341732131!5m2!1sen!2sph";
     info = `
       <h2>Kape Benito Community Cafe</h2>
-      <img src="/foodspot/benito.jpg" alt="Kape Benito Community Cafe" style="width: 100%; height: 100%; max-height: 480px; border-radius: 10px">
+      <img src="foodspot/benito.jpg" alt="Kape Benito Community Cafe" style="width: 100%; height: 100%; max-height: 480px; border-radius: 10px">
       <p><strong>Address:</strong> 48 Sparrow, Marikina</p>
       <p><strong>Price Range:</strong> ₱200-400+</p>
       <p><strong>Description:</strong> A welcoming community cafe serving heartwarming soups, delicious sandwiches, and a curated selection of coffee and non-coffee beverages.</p>
@@ -145,7 +145,7 @@ function changeMap(place, element) {
     mapSrc = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d965.1175749987641!2d121.09669709999999!3d14.629224400000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b9924d082a07%3A0xca823e743bb2789e!2sMama%20Chits%20Coffee%20House!5e0!3m2!1sen!2sph!4v1757341804767!5m2!1sen!2sph";
     info = `
       <h2>Mama Chits Coffee House</h2>
-      <img src="/foodspot/mama.jpg" alt="Mama Chits Coffee House" style="width: 100%; height: 100%; max-height: 480px; border-radius: 10px">
+      <img src="foodspot/mama.jpg" alt="Mama Chits Coffee House" style="width: 100%; height: 100%; max-height: 480px; border-radius: 10px">
       <p><strong>Address:</strong> 278 J. P. Rizal St, Marikina</p>
       <p><strong>Price Range:</strong> ₱100-200+</p>
       <p><strong>Description:</strong> A legendary vintage-themed restaurant filled with retro memorabilia. Famous for their family-style servings of burgers, pasta, and pizza in a nostalgic setting.</p>
@@ -154,7 +154,7 @@ function changeMap(place, element) {
     mapSrc = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d573.8802123015334!2d121.09684934617175!3d14.622388006060849!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b90e022620cf%3A0x4f64ea72ab877188!2s1975%20Old-Fashioned%20Burgers!5e0!3m2!1sen!2sph!4v1757341702835!5m2!1sen!2sph";
     info = `
       <h2>1975 Old-Fashioned Burgers</h2>
-      <img src="/foodspot/1975.jpg" alt="1975 Old-Fashioned Burgers" style="width: 100%; height: 100%; max-height: 480px; border-radius: 10px">
+      <img src="foodspot/1975.jpg" alt="1975 Old-Fashioned Burgers" style="width: 100%; height: 100%; max-height: 480px; border-radius: 10px">
       <p><strong>Address:</strong> 26, Yamson, Marikina</p>
       <p><strong>Price Range:</strong> ₱200-400+</p>
       <p><strong>Description:</strong> The place to go for authentic, old-fashioned burgers grilled to perfection. They also offer crispy chicken wings and satisfying rice meals at great value.</p>
@@ -163,7 +163,7 @@ function changeMap(place, element) {
     mapSrc = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d811.5848112306624!2d121.09588138263777!3d14.623569957716306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b839be28bcd1%3A0x2aa90e3b62eaccf9!2sSally's%20Gotohan!5e0!3m2!1sen!2sph!4v1759759326408!5m2!1sen!2sph";
     info = `
       <h2>Sally's Gotohan</h2>
-      <img src="/foodspot/sally.jpg" alt="Sally's Gotohan" style="width: 100%; height: 100%; max-height: 480px; border-radius: 10px">
+      <img src="foodspot/sally.jpg" alt="Sally's Gotohan" style="width: 100%; height: 100%; max-height: 480px; border-radius: 10px">
       <p><strong>Address:</strong> 27 Diego Silang, Marikina</p>
       <p><strong>Price Range:</strong> ₱100-200+</p>
       <p><strong>Description:</strong> A local favorite for comfort food, specializing in hot and savory goto (rice porridge) paired with traditional Filipino side dishes like tokwa't baboy.</p>
@@ -381,4 +381,5 @@ document.onkeydown = function(e) {
   if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
     return false;
   }
+
 };
