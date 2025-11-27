445ac58cbbfc840e08731308ac588f14f9ecfd1d
Marikenyo <ongjullianrob27@gmail.com>
2025-11-27 09:49:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -357,29 +357,36 @@ if (themeToggleBtn) {
   });
 }
 
-document.addEventListener('contextmenu', function(e) {
-  e.preventDefault();
+// 1. Disable Right Click
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
 });
 
-document.onkeydown = function(e) {
-  // F12
-  if (e.keyCode == 123) {
-    return false;
-  }
-  
-  // Ctrl+Shift+I (Inspect)
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-    return false;
-  }
-  
-  // Ctrl+Shift+J (Console)
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-    return false;
-  }
-  
-  // Ctrl+U (View Source)
-  if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-    return false;
-  }
+// 2. Disable F12, Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+U
+document.onkeydown = function (e) {
+    // F12
+    if (event.keyCode == 123) {
+        return false;
+    }
+    
+    // Ctrl+Shift+I (Inspect)
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+        return false;
+    }
+    
+    // Ctrl+Shift+J (Console)
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+        return false;
+    }
 
-};
+    // Ctrl+Shift+C (Element Inspector)
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 67) {
+        return false;
+    }
+    
+    // Ctrl+U (View Source)
+    if (e.ctrlKey && e.keyCode == 85) {
+        return false;
+    }
+}
+
